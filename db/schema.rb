a232a75271cd9593a80cc083ef067e63a53b8e63
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-03-20 22:58:59 -0400

    Added page titles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308035033) do
+ActiveRecord::Schema.define(version: 20160308035359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20160308035033) do
     t.string   "fax1"
     t.string   "fax2"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
